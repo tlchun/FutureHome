49378a9dd2a3cb64182dc54e632265572dd7ebe2
hx <xi.he@clife.cn>
2021-02-20 10:35:52 +0800

    整理

--- library-base/src/main/java/com/goldze/base/MQTTService.java
+++ library-base/src/main/java/com/goldze/base/MQTTService.java
@@ -1,8 +1,5 @@
 package com.goldze.base;
 
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
@@ -10,7 +7,6 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Binder;
 import android.os.IBinder;
-import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import org.eclipse.paho.android.service.MqttAndroidClient;
@@ -32,9 +28,8 @@ public class MQTTService extends Service {
     private String userName = "device_1";
     private String passWord = "public";
     private static String clientId = "device_1";//客户端标识
-    private static String myTopic = "/device/"+clientId;      //要订阅的主题
+    private static String myTopic = "/device/" + clientId;      //要订阅的主题
     private IGetMessageCallBack IGetMessageCallBack;
-
 
     @Override
     public void onCreate() {
@@ -43,12 +38,12 @@ public class MQTTService extends Service {
         init();
     }
 
-    public static void publish(String msg){
+    public static void publish(String msg) {
         String topic = myTopic;
         Integer qos = 0;
         Boolean retained = false;
         try {
-            if (client != null){
+            if (client != null) {
                 client.publish(topic, msg.getBytes(), qos.intValue(), retained.booleanValue());
             }
         } catch (MqttException e) {
@@ -83,11 +78,9 @@ public class MQTTService extends Service {
         Integer qos = 0;
         Boolean retained = false;
         if ((!message.equals("")) || (!topic.equals(""))) {
-            // 最后的遗嘱
             // MQTT本身就是为信号不稳定的网络设计的，所以难免一些客户端会无故的和Broker断开连接。
             //当客户端连接到Broker时，可以指定LWT，Broker会定期检测客户端是否有异常。
             //当客户端异常掉线时，Broker就往连接时指定的topic里推送当时指定的LWT消息。
-
             try {
                 conOpt.setWill(topic, message.getBytes(), qos.intValue(), retained.booleanValue());
             } catch (Exception e) {
@@ -96,13 +89,10 @@ public class MQTTService extends Service {
                 iMqttActionListener.onFailure(null, e);
             }
         }
-
         if (doConnect) {
             doClientConnection();
         }
-
     }
-
 
     @Override
     public void onDestroy() {
@@ -115,7 +105,9 @@ public class MQTTService extends Service {
         super.onDestroy();
     }
 
-    /** 连接MQTT服务器 */
+    /**
+     * 连接MQTT服务器
+     */
     private void doClientConnection() {
         if (!client.isConnected() && isConnectIsNormal()) {
             try {
@@ -124,18 +116,16 @@ public class MQTTService extends Service {
                 e.printStackTrace();
             }
         }
-
     }
 
     // MQTT是否连接成功
     private IMqttActionListener iMqttActionListener = new IMqttActionListener() {
-
         @Override
         public void onSuccess(IMqttToken arg0) {
             Log.i(TAG, "连接成功 ");
             try {
                 // 订阅myTopic话题
-                client.subscribe(myTopic,1);
+                client.subscribe(myTopic, 1);
             } catch (MqttException e) {
                 e.printStackTrace();
             }
@@ -151,12 +141,10 @@ public class MQTTService extends Service {
 
     // MQTT监听并且接受消息
     private MqttCallback mqttCallback = new MqttCallback() {
-
         @Override
         public void messageArrived(String topic, MqttMessage message) throws Exception {
-
             String str1 = new String(message.getPayload());
-            if (IGetMessageCallBack != null){
+            if (IGetMessageCallBack != null) {
                 IGetMessageCallBack.setMessage(str1);
             }
             String str2 = topic + ";qos:" + message.getQos() + ";retained:" + message.isRetained();
@@ -175,7 +163,9 @@ public class MQTTService extends Service {
         }
     };
 
-    /** 判断网络是否连接 */
+    /**
+     * 判断网络是否连接
+     */
     private boolean isConnectIsNormal() {
         ConnectivityManager connectivityManager = (ConnectivityManager) this.getApplicationContext()
                 .getSystemService(Context.CONNECTIVITY_SERVICE);
@@ -190,39 +180,19 @@ public class MQTTService extends Service {
         }
     }
 
-
     @Override
     public IBinder onBind(Intent intent) {
         Log.e(getClass().getName(), "onBind");
         return new CustomBinder();
     }
 
-    public void setIGetMessageCallBack(IGetMessageCallBack IGetMessageCallBack){
+    public void setIGetMessageCallBack(IGetMessageCallBack IGetMessageCallBack) {
         this.IGetMessageCallBack = IGetMessageCallBack;
     }
 
     public class CustomBinder extends Binder {
-        public MQTTService getService(){
+        public MQTTService getService() {
             return MQTTService.this;
         }
-    }
-
-    public  void toCreateNotification(String message){
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 1, new Intent(this,MQTTService.class), PendingIntent.FLAG_UPDATE_CURRENT);
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);//3、创建一个通知，属性太多，使用构造器模式
-
-        Notification notification = builder
-                .setTicker("测试标题")
-                .setSmallIcon(R.mipmap.ic_launcher)
-                .setContentTitle("")
-                .setContentText(message)
-                .setContentInfo("")
-                .setContentIntent(pendingIntent)//点击后才触发的意图，“挂起的”意图
-                .setAutoCancel(true)        //设置点击之后notification消失
-                .build();
-        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-        startForeground(0, notification);
-        notificationManager.notify(0, notification);
-
     }
 }
