15ae53139f48da774a4ba77b043d59cf5e92fba4
626787193@qq.com <Liuzhenhua135120>
2021-02-27 11:44:58 +0800

    源码依赖

---
+++ mvvmhabit/src/main/java/me/goldze/mvvmhabit/binding/viewadapter/radiogroup/ViewAdapter.java
@@ -0,0 +1,26 @@
+package me.goldze.mvvmhabit.binding.viewadapter.radiogroup;
+
+import androidx.annotation.IdRes;
+import androidx.databinding.BindingAdapter;
+import android.text.TextUtils;
+import android.webkit.WebView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import me.goldze.mvvmhabit.binding.command.BindingCommand;
+
+/**
+ * Created by goldze on 2017/6/18.
+ */
+public class ViewAdapter {
+    @BindingAdapter(value = {"onCheckedChangedCommand"}, requireAll = false)
+    public static void onCheckedChangedCommand(final RadioGroup radioGroup, final BindingCommand<String> bindingCommand) {
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, @IdRes int checkedId) {
+                RadioButton radioButton = (RadioButton) group.findViewById(checkedId);
+                bindingCommand.execute(radioButton.getText().toString());
+            }
+        });
+    }
+}
